04f76232d3f7adf663ea7ab2ee618432a113742e
S7nus <cicerorodrigo20@gmail.com>
2025-09-22 03:40:06 +0000

    Corrige erros de sintaxe e adiciona variáveis para cadastro de cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 int main(){
+  char estado1[1], estado2[1];
+  char codigo1[3], codigo2[3];
   char nome1[40], nome2[40];
   int populacao1, populacao2;
   float area1, area2;
   float pib1, pib2;
   int pontotur1, pontotur2;
 
-  printf ("Cadastre sua Carta" \n);
+  printf ("Cadastre sua Carta \n");
   printf ("Escolha uma letra de A a H");
-  scanf (%s, nome1);
+  scanf ("%s", estado1);
 
 }
