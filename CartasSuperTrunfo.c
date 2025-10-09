cfb04eefa9d2ebdd62242183285b2bdf8aa0ed65
S7nus <cicerorodrigo20@gmail.com>
2025-10-09 02:05:23 +0000

    MENU INTERATIVO COM SWITCH ESCOLHENDO COMPARAÇÕES NIVEL AVENTUREIRO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,6 +11,7 @@ int main(){
     float densidade1, densidade2;
     float pibpercapita1, pibpercapita2;
     float superpoder1, superpoder2;
+    int opcao;
 
     printf ("Bem vindo ao Super Trunfo de Estados Brasileiros!\n");
 
@@ -77,7 +78,99 @@ int main(){
     } else if (pib1 == pib2){
         printf("\nEmpate! Ambas as cartas têm o mesmo PIB.\n");
     }
+// MENU INTERATIVO
+    printf("\n===== COMPARE AS CARTAS =====\n");
+    printf("Escolha o atributo para comparar:\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("3 - PIB\n");
+    printf("4 - Pontos Turísticos\n");
+    printf("5 - Densidade Demográfica\n");
+    printf("6 - PIB per capita\n");
+    printf("7 - Super Poder\n");
+    printf("Sua escolha: ");
+    scanf("%d", &opcao);
 
+    printf("\nComparando %s e %s...\n", nome1, nome2);
+
+    // SWITCH COMPARATIVO
+    switch (opcao) {
+        case 1: // População
+            printf("População: %s = %lu | %s = %lu\n", nome1, populacao1, nome2, populacao2);
+            if (populacao1 > populacao2)
+                printf("Vencedor: %s\n", nome1);
+            else if (populacao2 > populacao1)
+                printf("Vencedor: %s\n", nome2);
+            else
+                printf("Empate!\n");
+            break;
+
+        case 2: // Área
+            printf("Área: %s = %.2f | %s = %.2f\n", nome1, area1, nome2, area2);
+            if (area1 > area2)
+                printf("Vencedor: %s\n", nome1);
+            else if (area2 > area1)
+                printf("Vencedor: %s\n", nome2);
+            else
+                printf("Empate!\n");
+            break;
+
+        case 3: // PIB
+            printf("PIB: %s = %.2f | %s = %.2f\n", nome1, pib1, nome2, pib2);
+            if (pib1 > pib2)
+                printf("Vencedor: %s\n", nome1);
+            else if (pib2 > pib1)
+                printf("Vencedor: %s\n", nome2);
+            else
+                printf("Empate!\n");
+            break;
+
+        case 4: // Pontos Turísticos
+            printf("Pontos Turísticos: %s = %d | %s = %d\n", nome1, pontotur1, nome2, pontotur2);
+            if (pontotur1 > pontotur2)
+                printf("Vencedor: %s\n", nome1);
+            else if (pontotur2 > pontotur1)
+                printf("Vencedor: %s\n", nome2);
+            else
+                printf("Empate!\n");
+            break;
+
+        case 5: // Densidade Demográfica (inverte a regra)
+            printf("Densidade: %s = %.2f | %s = %.2f\n", nome1, densidade1, nome2, densidade2);
+            if (densidade1 < densidade2)
+                printf("Vencedor: %s (menor densidade)\n", nome1);
+            else if (densidade2 < densidade1)
+                printf("Vencedor: %s (menor densidade)\n", nome2);
+            else
+                printf("Empate!\n");
+            break;
+
+        case 6: // PIB per capita
+            printf("PIB per capita: %s = %.6f | %s = %.6f\n", nome1, pibpercapita1, nome2, pibpercapita2);
+            if (pibpercapita1 > pibpercapita2)
+                printf("Vencedor: %s\n", nome1);
+            else if (pibpercapita2 > pibpercapita1)
+                printf("Vencedor: %s\n", nome2);
+            else
+                printf("Empate!\n");
+            break;
+
+        case 7: // Super Poder
+            printf("Super Poder: %s = %.2f | %s = %.2f\n", nome1, superpoder1, nome2, superpoder2);
+            if (superpoder1 > superpoder2)
+                printf("Vencedor: %s\n", nome1);
+            else if (superpoder2 > superpoder1)
+                printf("Vencedor: %s\n", nome2);
+            else
+                printf("Empate!\n");
+            break;
+
+        default:
+            printf("Opção inválida! Escolha um número de 1 a 7.\n");
+            break;
+    }
+
+    printf("\n===== Fim da Partida =====\n");
     
 
     
